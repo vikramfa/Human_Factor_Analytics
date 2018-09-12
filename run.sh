f3b23e13ff56366c62943ea6f44e77029c21541a
vigsrinivasan <Am4mindia6>
2018-09-12 20:18:16 +0530

    deleted some files

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-nameko run --config nameko.yml services.DataPublishService services.EARService services.ImagePublishService services.ImagePublishService2 services.HeartRateService services.GenderService services.EmotionService services.HeadPositionService&
-python earapp.py
-python hrapp.py
-python emotionapp.py
